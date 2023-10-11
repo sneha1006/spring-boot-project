1770fcb1ce2a159a4afded6bd26d762ed452cd7e
sneha <snehalakade1006@gmail.com>
2023-10-11 23:51:08 +0530

    springB project

---
+++ software/demo-springboot-project/src/main/java/com/assignment/springboot/project/demospringbootproject/DemoSpringbootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.assignment.springboot.project.demospringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootProjectApplication.class, args);
+		
+	}
+
+}
---
+++ software/demo-springboot-project/src/test/java/com/assignment/springboot/project/demospringbootproject/DemoSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.springboot.project.demospringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
